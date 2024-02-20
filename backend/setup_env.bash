1b72f3a17bb525782f8dc1aa7a9b1492b029b237
aboubakr <c.aboubakr@gmail.com>
2024-02-20 23:15:50 +0100

    update the file setup_env.bash

--- backend/setup_env.bash
+++ backend/setup_env.bash
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
+
+cat sql_dumps/setup_mysql_database.sql | sudo mysql -uroot -p
+cat cat backup_workhubconnect20022024.sql | sudo mysql -uroot -p workhubconnect_db
 WORKHUB_MYSQL_USER="workhub_user" WORKHUB_MYSQL_PWD="123" WORKHUB_MYSQL_HOST="localhost" WORKHUB_MYSQL_DB="workhubconnect_db"
 WORKHUB_MYSQL_USER="workhub_user" WORKHUB_MYSQL_PWD="123" WORKHUB_MYSQL_HOST="localhost" WORKHUB_MYSQL_DB="workhubconnect_db" python3
 WORKHUB_MYSQL_USER="workhub_user" WORKHUB_MYSQL_PWD="123" WORKHUB_MYSQL_HOST="localhost" WORKHUB_MYSQL_DB="workhubconnect_db" python3 -m api.v1.app
 find . -type d -name '__pycache__' -exec rm -r {} +
+
+
+WORKHUB_MYSQL_USER="workhub_user" WORKHUB_MYSQL_PWD="123" WORKHUB_MYSQL_HOST="localhost" WORKHUB_MYSQL_DB="workhubconnect_db" python3 -m auth.authen
