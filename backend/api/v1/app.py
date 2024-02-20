9d674e50bbdeecb9cdadd25bff81b268a91ce8ed
aboubakr <c.aboubakr@gmail.com>
2024-02-20 23:15:42 +0100

    update the file api/v1/app.py

--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -6,11 +6,16 @@ from flask import Flask
 from flask_cors import CORS
 from api.v1.views import app_views
 from models import storage
+from datetime import timedelta
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 # Create a Flask app
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
+CORS(app, supports_credentials=True)
+app.config["SECRET_KEY"] = "Bla-Bla"
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)
+jwt = JWTManager(app)
 # Register the blueprint for API routes
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
@@ -30,4 +35,4 @@ def not_found(EXP):
 if __name__ == "__main__":
     HOST = os.getenv('WORKHUB_API_HOST', "0.0.0.0")
     PORT = int(os.getenv('WORKHUB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
