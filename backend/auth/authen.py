2a2b13bfbf79fbc359bb89e29a328c03409bb79e
aboubakr <c.aboubakr@gmail.com>
2024-02-20 21:52:31 +0100

    update the file auth/authen.py

--- backend/auth/authen.py
+++ backend/auth/authen.py
@@ -5,29 +5,39 @@ from datetime import timedelta
 from models.user import User
 from hashlib import md5
 from models import storage
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
+CORS(app, supports_credentials=True)
 app.config["SECRET_KEY"] = "Bla-Bla"
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)
 jwt = JWTManager(app)
 
-@app.route('/token', methods=["POST"])
+@app.route('/login', methods=["POST"])
 def create_token():
     """generate new token base on User"""
     data = request.get_json()
     if data:
+        print(data)
         email = data.get("email", None)
         password = data.get("password", None)
         if email and password:
             PASSWORD = md5(password.encode()).hexdigest()
             user = storage.GetUserEmail(User, email=email, password=PASSWORD)
             if user is None:
+                print("bad email or password")
                 return make_response(jsonify({"error": "bad user or password"}), 401)
             generate_token = create_access_token(identity=user.id)
+            print("login success", generate_token)
             return make_response(jsonify({"token": generate_token, "user_id": user.id}))
-    return "not json", 400
+        if not email or len(email) == 0:
+            return make_response(jsonify({"error": "Eamil required"}), 401)
+        if not password or len(password) == 0:
+            return make_response(jsonify({"error": "Password required"}), 401)
+    else:
+        return "Not json", 400
 
 @app.route("/protected", methods=["GET"])
 @jwt_required()
