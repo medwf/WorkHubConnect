0f089a024ac9131690f80dd7f2a100c3ecb52ea9
aboubakr <c.aboubakr@gmail.com>
2024-02-24 15:59:57 +0100

    update the file setup_env.bash

--- backend/setup_env.bash
+++ backend/setup_env.bash
@@ -2,6 +2,7 @@ note
 app key for gmail:
 "avvv ogen hzzu mypj"
 export EMAIL_PASSWORD="avvv ogen hzzu mypj"
+SELECT * FROM cities  where name like "%‎%" LIMIT 400
 cat sql_dumps/setup_mysql_database.sql | sudo mysql -uroot -p
 cat cat backup_workhubconnect20022024.sql | sudo mysql -uroot -p workhubconnect_db
 WORKHUB_MYSQL_USER="workhub_user" WORKHUB_MYSQL_PWD="123" WORKHUB_MYSQL_HOST="localhost" WORKHUB_MYSQL_DB="workhubconnect_db"
