a4dfa88a2897accd15e8ac19aee31600fb7a089f
aboubakr <c.aboubakr@gmail.com>
2024-03-02 15:24:20 +0100

    update the file backend.sh

--- backend/backend.sh
+++ backend/backend.sh
@@ -7,9 +7,14 @@ sudo apt-get install mysql-server
 sudo apt-get install python3
 sudo apt-get install python3-pip
 
-# Install Flask and Flask-CORS
+# Install Flask and Flask-CORS, flasgger, flask_jwt_extended
 pip3 install Flask
 pip3 install Flask-CORS
+pip3 install flasgger
+pip3 install flask_jwt_extended
+
+#Install magic
+pip3 install python-magic
 
 # Install Sendmail
 sudo apt-get install sendmail
@@ -19,6 +24,7 @@ pip3 install SQLAlchemy
 
 # Optional: Install MySQL driver for SQLAlchemy
 pip3 install mysql-connector-python
+pip install pymysql
 
 # Optional: Install additional packages for email handling
 pip3 install secure-smtplib
@@ -35,3 +41,16 @@ FLUSH PRIVILEGES;" | sudo mysql -u root -p
 
 
 cat sql_dumps/backup_workhubconnect02032024.sql | sudo mysql -u root -p workhubconnect_db
+
+
+#ERROR
+	# /WorkHubConnect/backend/models/engine/db_storage.py", line 37, in __init__
+	#    self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}?charset=utf8mb4'.
+	#   File "<string>", line 2, in create_engine
+	#   ...
+	#   ModuleNotFoundError: No module named 'MySQLdb'
+	
+#SOLUTION :
+	# In the file /WorkHubConnect/backend/models/engine/db_storage.py 
+	# In this line  : self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}?charset=utf8mb4'.
+	# Change : mysql+mysqldb by mysql+pymysql
