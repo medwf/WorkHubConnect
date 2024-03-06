91a91087c60f9641d8130c0efd8a0f67c38bdbb0
med.wf <med.wf95@gmail.com>
2024-03-06 20:14:13 +0100

    add app.py

--- backend/api/v1/app.py
+++ backend/api/v1/app.py
@@ -9,7 +9,6 @@ from models import storage
 from datetime import timedelta
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flasgger import Swagger
-from flasgger.utils import swag_from
 
 
 # Create a Flask app
@@ -39,8 +38,12 @@ app.config['SWAGGER'] = {
     'title': 'WorkHubConnect Restful API',
     'uiversion': 3
 }
-
-Swagger(app)
+swagger_config = Swagger.DEFAULT_CONFIG
+swagger_config['swagger_ui_bundle_js'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js'
+swagger_config['swagger_ui_standalone_preset_js'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui-standalone-preset.js'
+swagger_config['jquery_js'] = '//unpkg.com/jquery@2.2.4/dist/jquery.min.js'
+swagger_config['swagger_ui_css'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui.css'
+Swagger(app, config=swagger_config)
 
 if __name__ == "__main__":
     HOST = os.getenv('WORKHUB_API_HOST', "0.0.0.0")
