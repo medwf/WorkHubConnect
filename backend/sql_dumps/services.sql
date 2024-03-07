fd56a25bb1b0f45eebc445c6e90953112baceb0c
aboubakr <c.aboubakr@gmail.com>
2024-03-07 15:14:40 +0100

    update the file sql_dumps/services.sql

--- backend/sql_dumps/services.sql
+++ backend/sql_dumps/services.sql
@@ -1,15 +1,15 @@
 
 INSERT INTO `services` (`id`, `image`, `en_name`, `ar_name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'images/services/Plomber.jpeg', 'Plumber', 'سباك', 'Handles installation and repair of water pipes, drainage systems, and fixtures like sinks and toilets.', "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(2, 'images/services/Electrician.jpg', 'Electrician', 'كهربائي', "Installs and repairs electrical systems, outlets, switches, and lighting fixtures.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(3, 'images/services/carpenter-woodworker.jpg', 'Carpenter', 'نجار', "Constructs, installs, and repairs wooden structures, furniture, cabinets, and shelves.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(4, 'images/services/House painting.jpg', 'House painter', 'صباغ', "Applies paint, stain, and other finishes to walls, ceilings, and other surfaces.","2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(5, 'images/services/HVAC Technician.jpg', 'HVAC Technician', 'فني تكييف وتبريد', "Installs, repairs, and maintains heating, ventilation, and air conditioning systems.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(6, 'images/services/Cleaner-Housekeeper.jpg', 'Cleaner/Housekeeper', 'نظافة/خادمة', "Cleans and tidies up houses, including dusting, vacuuming, mopping, and other general cleaning tasks.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(7, 'images/services/Gardener.jpg', 'Gardener/Landscaper', 'بستاني/مهندس تنسيق حدائق', "Maintains outdoor spaces, including lawn care, landscaping, pruning, and planting.","2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(8, 'images/services/Mover.jpeg', 'Mover', 'ناقل', "Helps with packing, loading, transporting, and unpacking household items during a move.","2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(9, 'images/services/Interior Designer.jpg',  'Interior Designer', 'مصمم داخلي', "Offers expertise in designing and decorating the interior spaces of houses to enhance functionality and aesthetics.","2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(10,'images/services/Event organizer.jpg',  'Event organizer', 'منظم حفلات', "Coordinates and plans events, managing logistics, vendors, and schedules to ensure successful gatherings and celebrations", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(11,'images/services/estate officer.jpg',  'Home Inspector', 'منعش عقاري', "Conducts inspections to assess the condition of a house, identifying any issues or potential problems.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(12, 'images/services/Home Appliance Technician.jpg', 'Home Appliance Technician', 'مصلح أجهزة كهربائية', "Repairs and maintains household appliances such as refrigerators, washing machines, dishwashers, and ovens.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
-(13,'images/services/House Plaster.png',  'House Plaster', 'جباص', "Installs, repairs, and maintains Plaster", "2024-02-13 19:26:17", "2024-02-13 19:26:17");
+(1, 'services/Plomber.jpeg', 'Plumber', 'سباك', 'Handles installation and repair of water pipes, drainage systems, and fixtures like sinks and toilets.', "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(2, 'services/Electrician.jpg', 'Electrician', 'كهربائي', "Installs and repairs electrical systems, outlets, switches, and lighting fixtures.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(3, 'services/carpenter-woodworker.jpg', 'Carpenter', 'نجار', "Constructs, installs, and repairs wooden structures, furniture, cabinets, and shelves.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(4, 'services/House_painting.jpg', 'House painter', 'صباغ', "Applies paint, stain, and other finishes to walls, ceilings, and other surfaces.","2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(5, 'services/HVAC_Technician.jpg', 'HVAC Technician', 'فني تكييف وتبريد', "Installs, repairs, and maintains heating, ventilation, and air conditioning systems.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(6, 'services/Cleaner-Housekeeper.jpg', 'Cleaner/Housekeeper', 'نظافة/خادمة', "Cleans and tidies up houses, including dusting, vacuuming, mopping, and other general cleaning tasks.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(7, 'services/Gardener.jpg', 'Gardener/Landscaper', 'بستاني/مهندس تنسيق حدائق', "Maintains outdoor spaces, including lawn care, landscaping, pruning, and planting.","2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(8, 'services/Mover.jpeg', 'Mover', 'ناقل', "Helps with packing, loading, transporting, and unpacking household items during a move.","2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(9, 'services/Interior Designer.jpg',  'Interior Designer', 'مصمم داخلي', "Offers expertise in designing and decorating the interior spaces of houses to enhance functionality and aesthetics.","2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(10,'services/Event_organizer.jpg',  'Event organizer', 'منظم حفلات', "Coordinates and plans events, managing logistics, vendors, and schedules to ensure successful gatherings and celebrations", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(11,'services/estate_officer.jpg',  'Home Inspector', 'منعش عقاري', "Conducts inspections to assess the condition of a house, identifying any issues or potential problems.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(12, 'services/Home_Appliance_Technician.jpg', 'Home Appliance Technician', 'مصلح أجهزة كهربائية', "Repairs and maintains household appliances such as refrigerators, washing machines, dishwashers, and ovens.", "2024-02-13 19:26:17", "2024-02-13 19:26:17"),
+(13,'services/House_Plaster.png',  'House Plaster', 'جباص', "Installs, repairs, and maintains Plaster", "2024-02-13 19:26:17", "2024-02-13 19:26:17");
