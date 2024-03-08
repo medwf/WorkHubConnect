a9a182ba547419e229d146de0ca697e2cdf7004a
aboubakr <c.aboubakr@gmail.com>
2024-03-09 00:41:04 +0100

    update the file api/v1/views/index.py

--- backend/api/v1/views/index.py
+++ backend/api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is index views, route of blueprint"""
-
+from flask import jsonify, make_response
 from api.v1.views import app_views
 from models import storage
 from models.city import City
@@ -18,7 +18,15 @@ from models.project import Project
 @app_views.route("/status", strict_slashes=False, methods=["GET"])
 def status():
     """return status ok, 200"""
-    return {"status": "OK"}
+    response_data = {
+    "app_name": "WorkHubConnect",
+    "version": "1.0",
+    "description": "WorkHubConnect Reset Full API",
+    "year_founded": 2024,
+    "country":"Morocco",
+    "status": "OK",
+    }
+    return  make_response (jsonify(response_data), 200)
 
 
 @app_views.route("/stats", strict_slashes=False, methods=["GET"])
@@ -32,7 +40,7 @@ def stats():
     states = storage.count(State)
     images = storage.count(Image)
     services = storage.count(Service)
-    return {
+    return make_response (jsonify({
         "users": users,
         "workers": workers,
         "projects": projects,
@@ -41,4 +49,4 @@ def stats():
         "reviews": reviews,
         "states": states,
         "cities": cities
-    }
+    }), 200)
