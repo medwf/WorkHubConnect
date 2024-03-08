342f5d59d32a3aa1b6815140eb7d76219ff36a50
aboubakr <c.aboubakr@gmail.com>
2024-03-08 22:40:49 +0100

    update the file api/v1/views/users.py

--- backend/api/v1/views/users.py
+++ backend/api/v1/views/users.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Importing modules"""
-import hashlib
 
 from flask import jsonify, make_response, request
 
@@ -10,7 +9,11 @@ from models.user import User
 from models.city import City
 from models.state import State
 from models.service import Service
-import re
+from models.worker import Worker
+from flask_jwt_extended import jwt_required, get_jwt_identity
+import magic
+import re, os
+from utils.upload import generate_filename, check_image_size
 from flasgger.utils import swag_from
 
 email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
@@ -34,7 +37,6 @@ def users():
     for user in users:
         result.append(user.to_dict())
     return jsonify(result)
-
 
 
 @app_views.route("/users/<int:user_id>", strict_slashes=False, methods=["GET"])
@@ -189,7 +191,6 @@ def Create_user():
         return make_response(jsonify({"error": "Not a JSON"}), 400)
 
 
-
 @app_views.route("/users/page/", strict_slashes=False, methods=["GET"])
 @app_views.route("/users/page/<int:offset>", strict_slashes=False, methods=["GET"])
 # @swag_from('documentation/user/offset_user.yml', methods=['GET'])
@@ -201,3 +202,222 @@ def users_with_offset(offset=1):
     for user in users:
         result.append(user.to_dict())
     return jsonify(result), 200
+
+
+@app_views.route("/uploadprofile", strict_slashes=False, methods=["POST"])
+def upload_img():
+    if request.method == 'POST':
+        if 'files' not in request.files:
+            print("not file")
+            return make_response(jsonify({"message": "No selected file"}), 400)
+        file = request.files['files']
+        if file.filename == '':
+            return make_response(jsonify({"message": "No selected file"}))
+        if file.filename == '':
+            return make_response(jsonify({"message": "No No selected file"}))
+        allowed_extensions = ("png", "jpeg", "jpg")
+        allowed_mime_types = ("image/jpeg", "image/png", "image/jpg")
+        basename, file_extension = file.filename.rsplit('.', 1)
+        if file_extension.lower() not in allowed_extensions:
+            return make_response(jsonify({"message": "Please upload images in one of the following formats: PNG, JPEG, or JPG"}))
+
+        new_filename = generate_filename(file_extension, 5)
+        current_directory = os.getcwd()
+        file.save('images/workers/' + new_filename)
+
+        file_path = f"{current_directory}/images/workers/{new_filename}"
+        mime = magic.Magic(mime=True)
+        file_mime_type = mime.from_file(file_path)
+        if file_mime_type not in allowed_mime_types:
+            os.remove(file_path)
+            return make_response(jsonify({"message": "Please upload images in one of the following mime_types : PNG, JPEG, or JPG"}))
+        is_valid = check_image_size(file_path)
+        if not is_valid:
+            return jsonify({"message": "Image size must be less than 2MB"})
+        url_img = f"/backend/images/workers/{new_filename}"
+        # response = jsonify({"message": "Image uploaded successfully", "imgurl" :{url_img} }), 200
+        # url_img = f"/backend/images/{new_filename}"
+        response = jsonify({"message": "Image uploaded successfully", "imgurl": {"url": url_img}}), 200
+        return response
+
+
+
+@app_views.route("/updateprofile", strict_slashes=False, methods=["PUT"])
+@jwt_required()
+def update_client_worker():
+    """
+    Update User :
+
+    If the HTTP body request is not valid JSON,
+    raise a 400 error with the message Not a JSON
+    Returns: the message with the status code 200
+    """
+
+    json_data = request.get_json(force=True, silent=True)
+    if json_data:
+        if len(json_data) > 0 and 'type' not in json_data or json_data['type'] not in ("client", "worker"):
+            # print("type not found")
+            return make_response(jsonify({"error": "Invalid request"}), 400)
+        user_id = get_jwt_identity()
+        user = storage.get(User, user_id)
+        if user is None:
+            return make_response(jsonify({"error": "User not found"}), 400)
+        if user.worker is None:
+            currenttype = "client"
+        else:
+            currenttype = "worker"
+
+        #update in case worker and still worker
+        if json_data['type'] == 'worker' and currenttype == 'worker':
+            user_keys = ['first_name', 'last_name', 'city_id', 'profile_img', 'phone_number', 'is_active']
+            user_data = {key: json_data[key] for key in user_keys if key in json_data}
+            worker_keys = ['user_id', 'service_id', 'city_id', 'description', 'diplome', 'certifications',
+                           'fb_url', 'insta_url','tiktok_url', 'linkedin_url', 'website_url']
+            worker_data = {key: json_data[key] for key in worker_keys if key in json_data}
+
+            if not storage.get(City, user_data.get('city_id', None)):
+                return make_response(jsonify({"error": "city not found"}), 400)
+            if len(user_data.get('first_name')) > 20 or len(user_data.get('first_name')) < 3:
+                return make_response(jsonify({"error": "Input first_name must be between 3 and 20 characters"}), 400)
+            if len(user_data.get('last_name')) > 20 or len(user_data.get('last_name')) < 3:
+                return make_response(jsonify({"error": "Input last_name must be between 3 and 20 characters"}), 400)
+            phone = user_data.get('phone_number', "")
+            if len(phone) > 0:
+                Phone = phone.replace(" ", "")
+                if not is_valid_phone_number(Phone):
+                    return make_response(jsonify({"error": "invalid phone Number most be (+212..) or (06...) or (07..) or (05...)"}), 400)
+                if len(Phone) > 16:
+                    return make_response(jsonify({"error": "Input phone_number must be less than 16 characters"}), 400)
+                user_data['phone_number'] = Phone
+            user.first_name = user_data.get("first_name", user.first_name)
+            user.last_name = user_data.get("last_name", user.last_name)
+            user.phone_number = user_data.get("phone_number", user.phone_number)
+            user.is_active = user_data.get("is_active", user.is_active)
+            user.city_id = user_data.get("city_id", user.city_id)
+            service_id = worker_data["service_id"]
+            if not storage.get(Service, service_id):
+                return make_response(jsonify({"error": "Service not found"}), 404)
+            worker = user.worker
+            worker.service_id = worker_data.get("service_id", worker.service_id)
+            worker.certifications = worker_data.get("certifications", worker.certifications)
+            worker.description = worker_data.get("description", worker.description)
+            worker.diplome = worker_data.get("diplome", worker.diplome)
+            worker.fb_url = worker_data.get("fb_url", worker.fb_url)
+            worker.insta_url = worker_data.get("insta_url", worker.insta_url)
+            worker.tiktok_url = worker_data.get("tiktok_url", worker.tiktok_url)
+            worker.linkedin_url = worker_data.get("linkedin_url", worker.linkedin_url)
+            # worker.is_available = worker_data.get("is_available", worker.is_available)
+            worker.website_url = worker_data.get("website_url", worker.website_url)
+            user.save()
+            worker.save()
+            return make_response(jsonify({"message": "Profile Updated Successfully"}), 200)
+
+
+        # update in case client and still client
+        if json_data['type'] == 'client' and currenttype == 'client':
+            user_keys = ['first_name', 'last_name', 'city_id', 'profile_img', 'phone_number', 'is_active']
+            user_data = {key: json_data[key] for key in user_keys if key in json_data}
+
+            if not storage.get(City, user_data.get('city_id', None)):
+                return make_response(jsonify({"error": "city not found"}), 400)
+            if len(user_data.get('first_name')) > 20 or len(user_data.get('first_name')) < 3:
+                return make_response(jsonify({"error": "Input first_name must be between 3 and 20 characters"}), 400)
+            if len(user_data.get('last_name')) > 20 or len(user_data.get('last_name')) < 3:
+                return make_response(jsonify({"error": "Input last_name must be between 3 and 20 characters"}), 400)
+            phone = user_data.get('phone_number', "")
+            if len(phone) > 0:
+                Phone = phone.replace(" ", "")
+                if not is_valid_phone_number(Phone):
+                    return make_response(jsonify({"error": "invalid phone Number most be (+212..) or (06...) or (07..) or (05...)"}), 400)
+                if len(Phone) > 16:
+                    return make_response(jsonify({"error": "Input phone_number must be less than 16 characters"}), 400)
+                user_data['phone_number'] = Phone
+            user.first_name = user_data.get("first_name", user.first_name)
+            user.last_name = user_data.get("last_name", user.last_name)
+            user.phone_number = user_data.get("phone_number", user.phone_number)
+            user.is_active = user_data.get("is_active", user.is_active)
+            user.city_id = user_data.get("city_id", user.city_id)
+            user.save()
+            return make_response(jsonify({"message": "Profile Updated Successfully"}), 200)
+
+
+        #update in case client become a worker
+        if json_data['type'] == 'worker' and currenttype == 'client':
+            user_keys = ['first_name', 'last_name', 'city_id', 'profile_img', 'phone_number', 'is_active']
+            user_data = {key: json_data[key] for key in user_keys if key in json_data}
+            worker_keys = ['user_id', 'service_id', 'city_id', 'description', 'diplome', 'certifications',
+                           'fb_url', 'insta_url','tiktok_url', 'linkedin_url', 'website_url']
+            worker_data = {key: json_data[key] for key in worker_keys if key in json_data}
+
+            if not storage.get(City, user_data.get('city_id', None)):
+                return make_response(jsonify({"error": "city not found"}), 400)
+            if len(user_data.get('first_name')) > 20 or len(user_data.get('first_name')) < 3:
+                return make_response(jsonify({"error": "Input first_name must be between 3 and 20 characters"}), 400)
+            if len(user_data.get('last_name')) > 20 or len(user_data.get('last_name')) < 3:
+                return make_response(jsonify({"error": "Input last_name must be between 3 and 20 characters"}), 400)
+            phone = user_data.get('phone_number', "")
+            if len(phone) > 0:
+                Phone = phone.replace(" ", "")
+                if not is_valid_phone_number(Phone):
+                    return make_response(jsonify({"error": "invalid phone Number most be (+212..) or (06...) or (07..) or (05...)"}), 400)
+                if len(Phone) > 16:
+                    return make_response(jsonify({"error": "Input phone_number must be less than 16 characters"}), 400)
+                user_data['phone_number'] = Phone
+            user.first_name = user_data.get("first_name", user.first_name)
+            user.last_name = user_data.get("last_name", user.last_name)
+            user.phone_number = user_data.get("phone_number", user.phone_number)
+            user.is_active = user_data.get("is_active", user.is_active)
+            user.city_id = user_data.get("city_id", user.city_id)
+            
+            if "service_id" not in worker_data:
+                return make_response(jsonify({"error": "Missing service_id"}), 400)
+            service_id = worker_data["service_id"]
+            if not storage.get(Service, service_id):
+                return make_response(jsonify({"error": "Service not found"}), 404)
+            worker_data['user_id'] = user_id
+            instance = Worker(**worker_data)
+            instance.save()
+            user.save()
+            return make_response(jsonify({"message": "Profile Updated Successfully"}), 200)
+
+
+        #update in case worker become a client
+        if json_data['type'] == 'client' and currenttype == 'worker':
+            user_keys = ['first_name', 'last_name', 'city_id', 'profile_img', 'phone_number', 'is_active']
+            user_data = {key: json_data[key] for key in user_keys if key in json_data}
+
+            if not storage.get(City, user_data['city_id']):
+                return make_response(jsonify({"error": "city not found"}), 400)
+            if len(user_data.get('first_name')) > 20 or len(user_data.get('first_name')) < 3:
+                return make_response(jsonify({"error": "Input first_name must be between 3 and 20 characters"}), 400)
+            if len(user_data.get('last_name')) > 20 or len(user_data.get('last_name')) < 3:
+                return make_response(jsonify({"error": "Input last_name must be between 3 and 20 characters"}), 400)
+            phone = user_data.get('phone_number', "")
+            if len(phone) > 0:
+                Phone = phone.replace(" ", "")
+                if not is_valid_phone_number(Phone):
+                    return make_response(jsonify({"error": "invalid phone Number most be (+212..) or (06...) or (07..) or (05...)"}), 400)
+                if len(Phone) > 16:
+                    return make_response(jsonify({"error": "Input phone_number must be less than 16 characters"}), 400)
+                user_data['phone_number'] = Phone
+            user.first_name = user_data.get("first_name", user.first_name)
+            user.last_name = user_data.get("last_name", user.last_name)
+            user.phone_number = user_data.get("phone_number", user.phone_number)
+            user.is_active = user_data.get("is_active", user.is_active)
+            user.city_id = user_data.get("city_id", user.city_id)
+            user.save()
+            worker = user.worker
+            for project in worker.projects:
+                for image in project.images:
+                    img_url = image.url
+                    full_path =  f"{os.getcwd()}{img_url}"
+                    os.remove(full_path)
+                    storage.delete(image)
+                storage.delete(project)
+            for review in worker.reviews:
+                storage.delete(review)
+            storage.delete(worker)
+            storage.save()
+            return make_response(jsonify({"message": "Profile Updated Successfully, You are no longer designated as a worker."}), 200)
+    else:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
